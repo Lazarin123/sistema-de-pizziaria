2e71deb1e3f1681c589c0a8d4fc97400b71db7b2
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-04-01 22:52:55 -0300

    estrutura If e Else

--- sistema.c
+++ sistema.c
@@ -7,5 +7,31 @@ int main() {
     printf("Qual tamanho deseja? \n");
     scanf("%d", &tamanho);
 
+    char qtdsabor;
+    printf("(1)-Inteira (2)-Meia a Meia");
+    printf("Qual é o modelo? ");
+    scanf("%c", &qtdsabor);
+
+    if (qtdsabor == 1)
+    {
+        int sabor;
+        printf("(1)-Mussarela\n");
+        printf("(2)-Calabresa\n");
+        printf("(3)-Portuguesa\n");
+        printf("(4)-Franco C/ Catupry\n");
+        printf("(5)-Peperoni\n");
+        printf("Escolha seu Sabor: \n");
+        scanf("%d", &sabor);
+    } else {
+        int sabor;
+        printf("(1)-Mussarela\n");
+        printf("(2)-Calabresa\n");
+        printf("(3)-Portuguesa\n");
+        printf("(4)-Franco C/ Catupry\n");
+        printf("(5)-Peperoni\n");
+        printf("Escolha seu Sabor: \n");
+        scanf("%d", &sabor);
+    }
     
+
 }
