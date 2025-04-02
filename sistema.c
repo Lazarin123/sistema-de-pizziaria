f34cc7c509f7408eb51a974d387e8e02970f975f
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-04-01 23:01:26 -0300

    termino das perguntas e respostas

--- sistema.c
+++ sistema.c
@@ -9,7 +9,7 @@ int main() {
 
     int modelosabor;
     printf("(1)-Inteira (2)-Meia a Meia\n");
-    printf("Qual é o modelo? \n");
+    printf("Qual eh o modelo? \n");
     scanf("%d", &modelosabor);
 
     if (modelosabor == 1)
@@ -23,19 +23,29 @@ int main() {
         printf("Escolha seu Sabor: \n");
         scanf("%d", &sabor);
     } else {
-        int sabor;
+        int sabor1;
         printf("(1)-Mussarela\n");
         printf("(2)-Calabresa\n");
         printf("(3)-Portuguesa\n");
         printf("(4)-Franco C/ Catupry\n");
         printf("(5)-Peperoni\n");
-        printf("Escolha seu Sabor: \n");
-        scanf("%d", &sabor);
+        printf("Escolha o seu primeiro Sabor: \n");
+        scanf("%d", &sabor1);
+
+        int sabor2;
+        printf("(1)-Mussarela\n");
+        printf("(2)-Calabresa\n");
+        printf("(3)-Portuguesa\n");
+        printf("(4)-Franco C/ Catupry\n");
+        printf("(5)-Peperoni\n");
+        printf("Escolha o seu Segundo Sabor: \n");
+        scanf("%d", &sabor2);
     }
 
     int borda;
     printf("(1)-Sim (2)-Nao\n");
     printf("Deseja adicionar borda de Catupry? \n");
     scanf("%d", &borda);
+
 
 }
