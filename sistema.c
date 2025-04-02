c06000f7211c56d8364e7c64fbcee5e1c62e5d88
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-04-01 22:33:31 -0300

    estrutura da pergunta do tamanho da pizza.

--- sistema.c
+++ sistema.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main() {
+    int tamanho;
+
+    printf("(1)-Pequena (2)-Média (3)-Grande");
+    printf("Qual tamanho deseja? \n");
+    scanf("%d", &tamanho);
+
     
 }
